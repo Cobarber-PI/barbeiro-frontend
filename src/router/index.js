c0c0d269357e343ba932874eb02ab5efd9da0542
goodsir6 <eduardo.bomsenhor.info@gmail.com>
2025-11-03 16:34:40 -0300

    feat: ganhos e barbeiros #2

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/ganhos',
+      name: 'ganhos',
+      component: () => import('../views/GanhosView.vue'),
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
---
+++ src/views/GanhosView.vue
@@ -0,0 +1,6 @@
+<script setup> 
+import Ganhos from '@/components/Ganhos.vue';
+</script>
+<template>
+    <Ganhos />
+</template>
