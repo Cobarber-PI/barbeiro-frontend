c9274919688950ee68682d2f1c0e2456cfeebb33
goodsir6 <eduardo.bomsenhor.info@gmail.com>
2025-11-22 17:21:25 -0300

    Detalhe router

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/barbeiroApp',
+      path: '/',
       name: 'app',
       component: AppView,
     },
